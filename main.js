01c57899838625fd5a8fad42f9e729ec4abea263
Ece <74778171+kedimonmuo@users.noreply.github.com>
2022-02-15 15:55:41 +0100

    basic functions

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("hej!");
+
+var username= "";
+username= "Ece";
+
+var divh = "<div class = \"flexdiv\" ></div>"
+
+
+document.getElementById("root").innerHTML =  " <h1> Welcome to my webpage " + username +  "</h1>" ;
+
+console.log("2022-02-15");
+var a ="<h1>  hej hej </h1>";
+var b = "<h2> Jag heter Ece</h2>";
+var c = "<h3> Snart kommer mer information </h3>";
+
+b = "<h2> Ipek heter jag </h2>";
+
+function texter(t,m,x){
+var f = t + m + x;
+return f;
+}
+
+document.getElementById("root").innerHTML += X(" HEJ HEJ" + a);
+
+const  cars = [ "saab", "volvo", "bmw"];
+function X(a) {
+
+
+};
+X("cars[1]");
+
+
+const names = ["1-ece", "2-maricel" ,"3-emmie", "4-anton","5-kerem" ,"6-dimitrij", "7-ahmad" ,"8-kristof", "9-fredrik" , "10-surjata", "11-serzat" ,"12-narin", "13-isabel", "14-ayat", "15-patrik", "16-ipek", "17-norreine", "18-johan", "19-sevin","20-minnak"];
+
+
+names.forEach(function(Y) {
+  console.log(Y);
+  document.getElementById("X").innerHTML += Y;
+});
